8002a501da275b86567ac6d0489cf39b75a41543
Varun Sharma <varunited@gmail.com>
2016-09-29 17:53:05 +0530

    Add new class Geometry::Point to initialize a point

--- lib/geometry/point.rb
+++ lib/geometry/point.rb
@@ -1,19 +1,39 @@
-class Geometry::Linesegment
-  attr_reader :point_1, :point_2
+class Geometry::Point
+  attr_reader :point
 
-  def initialize(x, y)
-    @point_1 = x
-    @point_2 = y
+  def initialize(abscissa, ordinate)
+    @point = { 'abscissa' => abscissa, 'ordinate' => ordinate}
   end
-
-  def length_between_2_points
-    if @point_1 == 0 && @point_2 == 0
-      0
-    else
-      Math.sqrt( ((@point_2[1] - @point_1[1]) ** 2).abs + ((@point_2[0] - @point_1[0]) ** 2).abs )
-    end
-  end
-
-  private
-  attr_writer :x_coordinate, :y_coordinate
 end
+
+
+# class Geometry::Linesegment
+#   attr_reader :point_1, :point_2
+#
+#   def initialize(x, y)
+#     @point_1 = x
+#     @point_2 = y
+#   end
+#
+#   def length_between_2_points
+#     if @point_1 == 0 && @point_2 == 0
+#       0
+#     else
+#       Math.sqrt( ((@point_2[1] - @point_1[1]) ** 2).abs + ((@point_2[0] - @point_1[0]) ** 2).abs )
+#     end
+#   end
+#
+#   private
+#   attr_writer :x_coordinate, :y_coordinate
+# end
+#
+# class Geometry::Point
+#   attr_reader :point
+#
+#   def initialize(x, y)
+#     @point_1 = x
+#     @point_2 = y
+#   end
+#
+# end
+#
--- spec/geometry/point_spec.rb
+++ spec/geometry/point_spec.rb
@@ -1,21 +1,31 @@
 require "spec_helper"
 
-describe Geometry::Linesegment do
-  describe "#length_between_2_points" do
-    it "should return 0 given (0,0) and (0,0)" do
-      length = Geometry::Linesegment.new([0,0], [0,0])
-      expect(length.length_between_2_points).to eq(0)
-    end
+# describe Geometry::Linesegment do
+#   describe "#length_between_2_points" do
+#     it "should return 0 given (0,0) and (0,0)" do
+#       length = Geometry::Linesegment.new([0,0], [0,0])
+#       expect(length.length_between_2_points).to eq(0)
+#     end
+#
+#     it "should return 3 given (1,3) and (1,0)" do
+#       length = Geometry::Linesegment.new([1,3], [1,0])
+#       expect(length.length_between_2_points).to eq(3)
+#     end
+#
+#     it "should return 5 given (-1,4) and (2,0)" do
+#       length = Geometry::Linesegment.new([-1,4], [2,0])
+#       expect(length.length_between_2_points).to eq(5)
+#     end
+#
+#   end
+# end
 
-    it "should return 3 given (1,3) and (1,0)" do
-      length = Geometry::Linesegment.new([1,3], [1,0])
-      expect(length.length_between_2_points).to eq(3)
+describe Geometry::Point do
+  describe "#point_initialization" do
+    it "should return point (0,0) when 0 and 0 are passed as abscissa and ordinate respectively" do
+      point = Geometry::Point.new(0,0)
+      expect(point.point['abscissa']).to eq(0)
+      expect(point.point['ordinate']).to eq(0)
     end
-
-    it "should return 5 given (-1,4) and (2,0)" do
-      length = Geometry::Linesegment.new([-1,4], [2,0])
-      expect(length.length_between_2_points).to eq(5)
-    end
-
   end
 end

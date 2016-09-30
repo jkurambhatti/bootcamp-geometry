641440810841cd98ea062808655f8cb2c1253c91
Varun Sharma <varunited@gmail.com>
2016-09-30 12:07:34 +0530

    Add default formatting for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,14 @@
 require "rspec"
 
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-  config.shared_context_metadata_behavior = :apply_to_host_groups
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
 end
 
 require_relative "../lib/geometry"
